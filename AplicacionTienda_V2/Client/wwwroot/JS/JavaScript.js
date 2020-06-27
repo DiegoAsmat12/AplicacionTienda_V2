27d12f76db5e32aa91752980d7a73dae0960896e
diegoas1205 <diegoas1205@gmail.com>
2020-06-27 16:07:18 -0500

    Avanzando La Tienda Virtual

---
+++ AplicacionTienda_V2/Client/wwwroot/JS/JavaScript.js
@@ -0,0 +1,17 @@
+﻿function cambiarImagenCarousel() {
+    if (document.getElementById("img-1").checked) {
+        document.getElementById("img-1").checked = false;
+        document.getElementById("img-2").checked = true;
+        return;
+    }
+    if (document.getElementById("img-2").checked) {
+        document.getElementById("img-2").checked = false;
+        document.getElementById("img-3").checked = true;
+        return;
+    }
+    if (document.getElementById("img-3").checked) {
+        document.getElementById("img-3").checked = false;
+        document.getElementById("img-1").checked = true;
+        return;
+    }
+}
